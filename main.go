e8909d7d77eabd7b5f1caeeb7c62183c027b3306
hexiufeng <rolandhe@qq.com>
2024-07-02 14:15:25 +0800

    add comment for main func

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ var (
 	role  = flag.String("role", "master", "实例角色, master or slave")
 	host  = flag.String("host", "", "master host")
 	port  = flag.Int("port", 8080, "master port")
-	seqId = flag.Int64("event-id", 0, "replica event id")
+	seqId = flag.Int64("event", 0, "replica event id")
 )
 
+// main
+// master mode: ./smss -role master
+// slave mode: ./smss -role slave -host 127.0.0.1 -port 8080 -event 0
 func main() {
 	if err := conf.Init(); err != nil {
 		return
