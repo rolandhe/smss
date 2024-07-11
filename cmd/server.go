c979d324c2721c180da7c6221fdffbe1f6f550ce
hexiufeng <rolandhe@qq.com>
2024-07-11 18:27:45 +0800

    add log

--- cmd/server.go
+++ cmd/server.go
@@ -125,12 +125,12 @@ func handleConnection(conn net.Conn, worker *backWorker) {
 		header, err := router.ReadHeader(conn)
 		if err != nil {
 			if nets.IsTimeoutError(err) {
+				continuesTimeoutCount++
 				if continuesTimeoutCount == 360 {
 					logger.Get().Infof("handleConnection no cmd more than 30 mins:%v", err)
 					return
 				}
-				logger.Get().Infof("handleConnection read header err:%v", err)
-				continuesTimeoutCount++
+				logger.Get().Infof("handleConnection times=%d, read header err:%v", continuesTimeoutCount, err)
 				continue
 			}
 			logger.Get().Infof("handleConnection read header err:%v", err)
